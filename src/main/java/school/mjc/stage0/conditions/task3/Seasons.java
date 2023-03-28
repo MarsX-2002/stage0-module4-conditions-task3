e3dfe713a8e871e826d4da68c722ef3734e9b1e6
MarsX-2002 <mirjalol0331@gmail.com>
2023-03-28 11:44:24 +0500

    if else

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task3;
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
         if (month == 12 || (month <=2 && month >= 1)){
-            System.out.println("wrong number!");
+            System.out.println("Winter");
         } else if (month >= 3 && month <= 5) {
             System.out.println("Spring");
         } else if (month >= 6 && month <= 8) {
